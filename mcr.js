4f3a9411ddec1797159fb559236af2f8a6f94a21
Matthew Rieger <matthewcreanrieger@gmail.com>
2017-05-22 12:27:45 -0400

    Construction 3

--- mcr.js
+++ mcr.js
@@ -22,3 +22,40 @@ function exitImage(e) {
    $('a', this).get(0).style.color = "white";
    $('a', this).get(1).style.color = "white";
 }
+
+var bkgImg = new Array (
+		"../assets/bkgImg/1.jpg",
+		"../assets/bkgImg/2.jpg",
+		"../assets/bkgImg/3.jpg",
+		"../assets/bkgImg/4.jpg",
+		"../assets/bkgImg/5.jpg",
+		"../assets/bkgImg/6.jpg",
+		"../assets/bkgImg/7.jpg",
+		"../assets/bkgImg/8.jpg",
+		"../assets/bkgImg/9.jpg",
+		"../assets/bkgImg/10.jpg",
+		"../assets/bkgImg/11.jpg",
+		"../assets/bkgImg/12.jpg",
+		"../assets/bkgImg/13.jpg",
+		"../assets/bkgImg/14.jpg",
+		"../assets/bkgImg/15.jpg",
+		"../assets/bkgImg/16.jpg",
+		"../assets/bkgImg/17.jpg",
+		"../assets/bkgImg/18.jpg",
+		"../assets/bkgImg/19.jpg",
+		"../assets/bkgImg/20.jpg",
+		"../assets/bkgImg/21.jpg",
+		"../assets/bkgImg/22.jpg",
+		"../assets/bkgImg/23.jpg",
+		"../assets/bkgImg/24.jpg"
+);
+var bkgImgIndex = 0;
+	
+function rndBkgImgDsply (x) {
+	bkgImgIndex = bkgImgIndex + x;
+	if (bkgImgIndex < 0) bkgImgIndex = bkgImg.length - 1;
+	if (bkgImgIndex > bkgImg.length - 1) bkgImgIndex = 0;
+	document.getElementById ( "bkgImg" ) .src = bkgImg[bkgImgIndex];
+}
+
+window.onload = rndBkgImgDsply (0);
