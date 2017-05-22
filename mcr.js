bfe1bc33151f97d1cec98a76185c0e194101c264
Matthew Rieger <matthewcreanrieger@gmail.com>
2017-05-22 11:09:20 -0400

    Testing mobile alert 2

--- mcr.js
+++ mcr.js
@@ -23,6 +23,6 @@ function exitImage(e) {
    $('a', this).get(1).style.color = "white";
 }
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) && window.location.href == 'index.html') {
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) && (window.location.href == 'http://www.matthewcreanrieger.com' || window.location.href == 'http://www.matthewcreanrieger.com/index.html')) {
 	alert("This site is only optimized for desktop computers;\nno mobile version is currently available.");
 }
